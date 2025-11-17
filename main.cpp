b14e38679b61abd505bc37c264a3fdd6b02f0dd6
Ashborn <codenomad18@gmail.com>
2025-11-17 02:20:11 +0200

    Enhance system info display with line limits
    Added line counting for CPU and memory info display. Updated OS version display to only show NAME and VERSION.

--- main.cpp
+++ main.cpp
@@ -2,27 +2,32 @@
 #include <string>
 #include <fstream>
 using namespace std;
-
 int main(){
     short int userOption;
     string line;
-
+    int lineCount; 
    do{
      cout << "1.Show Cpu info\n 2.Show memory info\n 3.Show Os version\n 4.Exit\n";
      cin >> userOption;
      switch(userOption){
         case 1: {
             ifstream cpuFile("/proc/cpuinfo");
+            lineCount = 0; 
             while(getline(cpuFile, line)){
                 cout << line << endl;
+                lineCount++;
+                if(lineCount >= 7) break; 
             }
             cpuFile.close();
             break;
         }
         case 2: {
             ifstream memoryFile("/proc/meminfo");
+            lineCount = 0; 
             while(getline(memoryFile, line)){
                 cout << line << endl;
+                lineCount++;
+                if(lineCount >= 3) break; 
             }
             memoryFile.close();
             break;
@@ -30,7 +35,9 @@ int main(){
         case 3: {
             ifstream osVersion("/etc/os-release");
             while(getline(osVersion, line)){
-                cout << line << endl;
+                if(line.find("NAME=") == 0 || line.find("VERSION=") == 0){
+                    cout << line << endl;
+                }
             }
             osVersion.close();
             break;
@@ -41,4 +48,4 @@ int main(){
         }
     }while(userOption != 4);
     return 0;
-}   
+}
